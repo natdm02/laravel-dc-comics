19695f74215801c7c007b62f418b7e0742c47a0d
natdm02 <natalidimarino2002@gmail.com>
2024-05-20 21:23:11 +0200

    [add]resolutions

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,16 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css' integrity='sha512-Z0kTB03S7BU+JFU0nw9mjSBcRnZm2Bvm0tzOX9/OuOuz01XQfOpa0w/N9u6Jf2f1OAdegdIPWZ9nIZZ+keEvBw==' crossorigin='anonymous'/>
     <!-- Styles -->
     @vite('resources/js/app.js')
 
     <title>laravek dc comics</title>
+    @vite('resources/js/app.js')
 </head>
 <body>
     @include('partials.header')
 
-    <main>
+    <main class="my-5">
         @yield('content')
     </main>
 
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
-<footer>
+<footer class="text-bg-dark py-4 text-center">
     <h1>Footer</h1>
 </footer>
