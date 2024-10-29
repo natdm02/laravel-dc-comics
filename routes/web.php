38c23b7e68feaac49678a27e26706dd0c5fcb2eb
natdm02 <natalidimarino2002@gmail.com>
2024-10-29 14:52:07 +0100

    [add] created the resource controller for comics

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+route::resource('comics', ComicController::class);
