3ccbd33cced70421cc7489d0ef69bc45459f1305
natdm02 <natalidimarino2002@gmail.com>
2024-10-31 12:03:12 +0100

    [add] delete function

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -77,6 +77,9 @@ class ComicController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $comic = Comic::findOrFail($id);
+        $comic->delete();
+
+    return redirect()->route('comics.index')->with('success', 'fumetto eliminato!');
     }
 }
