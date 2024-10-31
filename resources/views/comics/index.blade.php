c91d96f5c381537a92339822e29373ce072dc3d8
natdm02 <natalidimarino2002@gmail.com>
2024-10-31 12:05:00 +0100

    [add] updated index.blade to display and manage the list

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,14 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>LISTA FUMETTI</h1>
-    <a href="{{ route('comics.create') }}">Crea Nuovo Fumetto</a>
 
-    <ul>
-        @foreach ($comics as $comic)
-            <li>
-                <a href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a>
-            </li>
-        @endforeach
-    </ul>
+    <h1>Lista Fumetti</h1>
+
+    @if($comics->isEmpty())
+
+        <p>Nessun fumetto trovato.</p>
+
+    @else
+
+        <ul>
+
+            @foreach ($comics as $comic)
+
+                <li>
+                    <a href="{{ route('comics.show', $comic->id) }}">{{ $comic->title }}</a>
+                    <a href="{{ route('comics.edit', $comic->id) }}">Modifica</a>
+                    <form action="{{ route('comics.destroy', $comic->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Elimina</button>
+                    </form>
+
+                </li>
+            @endforeach
+
+        </ul>
+
+    @endif
 @endsection
+
