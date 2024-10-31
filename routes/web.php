4f84a01dc223e177f9e603902cde33ded7c0899c
natdm02 <natalidimarino2002@gmail.com>
2024-10-31 12:03:52 +0100

    [add]route resource

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComicController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-route::resource('comics', ComicController::class);
+Route::resource('comics', ComicController::class);
+
+Route::get('/', function () {
+    return redirect()->route('comics.index');
+});
